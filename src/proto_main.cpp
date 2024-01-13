bf72f89cdd1aa63df8cfacccfbfe38cdf3525b7d
Ben Hall <rcmast3r1@gmail.com>
2024-01-13 14:40:11 -0500

    removed some comments

--- src/proto_main.cpp
+++ src/proto_main.cpp
@@ -6,12 +6,7 @@
 
 #include "ADC_SPI.h"
 
-// // Sensors and hardware
-// ADC_SPI ADC1(ADC1_CS, ADC_SPI_SPEED, ECU_SDI, ECU_SDO, ECU_CLK);
-// ADC_SPI ADC2(ADC2_CS, ADC_SPI_SPEED, ECU_SDI, ECU_SDO, ECU_CLK);
-// ADC_SPI ADC3(ADC3_CS, ADC_SPI_SPEED, ECU_SDI, ECU_SDO, ECU_CLK);
-// // driver
-// // component
+
 
 BuzzerController buzzer(500);
 
