ef8cc7c3f01bc67dce380a7a522bc41027e109cb
Ben Hall <rcmast3r1@gmail.com>
2024-01-13 15:46:29 -0500

    adding in prototype of message handler system

--- src/proto_main.cpp
+++ src/proto_main.cpp
@@ -3,9 +3,10 @@
 // /* Include files */
 #include <Arduino.h>
 #include "MCUStateMachine.h"
+#include "FlexCAN_T4.h"
 
 #include "ADC_SPI.h"
-
+#include "MessageHandler.h"
 
 
 BuzzerController buzzer(500);
@@ -22,10 +23,18 @@ InverterComponent rr_inv(100);
 DrivetrainComponent drivetrain({&lf_inv, &rf_inv, &lr_inv, &rr_inv});
 MCUStateMachine state_machine(&buzzer, &drivetrain, &dash_interface);
 
+
+FlexCAN_T4<CAN2, RX_SIZE_256, TX_SIZE_16> FRONT_INV_CAN;
+CAN_message_t msg;
+MessageHandler<FlexCAN_T4_Base, CAN_message_t> msg_writer(&FRONT_INV_CAN, &msg, 100, millis());
+
+
 void setup() {
     
 }
 
 void loop() {
-    state_machine.tick_state_machine(millis());
+    msg_writer.handle_sending(millis());
+    // msg_writer.test();
+    // state_machine.get_state();
 }
