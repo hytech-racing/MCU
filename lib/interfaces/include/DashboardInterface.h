c7dcf1ef829edce5ba0988345e06d13d62cf8ec4
walkermburns <walkermburns@gmail.com>
2024-01-18 20:50:01 -0500

    Added member function definitions for DashboardInterface

--- lib/interfaces/include/DashboardInterface.h
+++ lib/interfaces/include/DashboardInterface.h
@@ -1,6 +1,8 @@
 #ifndef __DASHBOARDINTERFACE_H__
 #define __DASHBOARDINTERFACE_H__
 
+#include "Dashboard_status.h"
+
 enum DialMode_s
 {
     MODE_1,
@@ -11,17 +13,98 @@ enum DialMode_s
     ENDURANCE,
 };
 
+enum LEDColors_s
+{
+    OFF,
+    ON,
+    YELLOW,
+    RED,
+};
+
+struct DashButtons_s
+{
+    bool start;
+    bool mark;
+    bool mode; // torque mode
+    bool mc_cycle; // clears encoder error codes
+    bool launch_ctrl;
+    bool torque_mode; // torque loading
+    bool led_dimmer;
+};
+
+struct DashLEDs_s
+{
+    uint8_t ams;
+    uint8_t imd;
+    uint8_t mode;
+    uint8_t mc_error;
+    uint8_t start;
+    uint8_t inertia;
+    uint8_t mech_brake;
+    uint8_t gen_purp;
+    uint8_t bots;
+    uint8_t cockpit_brb;
+    uint8_t crit_charge;
+    uint8_t glv;
+    uint8_t launch_control;
+}
+
+struct DashComponentInterface
+{
+    /* READ DATA */
+    // enum for dial position read by controller mux
+    DialMode_s dial_mode;
+    // Buttons struct for better naming
+    DashButtons_s button;
+    bool ssok; // safety system OK (IMD?) RENAME
+    bool shutdown;
+
+    /* WRITTEN DATA */
+    bool buzzer;
+    DashLEDs_s LED;
+};
+
+
 class DashboardInterface
 {
 private:
+
+    DashboardInterface data;
+
 public:
-    virtual DialMode_s getDialMode();
-    virtual bool nightModeButtonPressed();
-    virtual bool inverterResetButtonPressed();
-    virtual bool torqueVectoringOffButtonPressed();
-    virtual bool startButtonPressed();
-    virtual bool torqueButtonPressed();
-    virtual bool specialButtonPressed();
+    Dashboard(){};
+
+    void read(const Dashboard_status &msg);
+
+    DialMode_s getDialMode();
+
+    bool startButtonPressed();
+    bool specialButtonPressed();
+    bool torqueButtonPressed();
+    bool inverterResetButtonPressed();
+    bool launchControlButtonPressed();
+    bool torqueLoadingButtonPressed();
+    bool nightModeButtonPressed();
+    bool torqueVectoringOffButtonPressed();
+
+    bool safetySystemOK();
+
+
+    void soundBuzzer();
+
+    void amsLED(LEDColors_s color);
+    void imdLED(LEDColors_s color);
+    void modeLED(LEDColors_s color);
+    void mcErrorLED(LEDColors_s color);
+    void startLED(LEDColors_s color);
+    void InertiaSwitchLED(LEDColors_s color);
+    void mechanicalBrakeLED(LEDColors_s color);
+    void generalPurposeLED(LEDColors_s color);
+    void botsLED(LEDColors_s color);
+    void cockpitBrbLED(LEDColors_s color);
+    void critChargeLED(LEDColors_s color);
+    void glvLED(LEDColors_s color);
+    void launchControlLED(LEDColors_s color);
 };
 
 #endif /* __DASHBOARDINTERFACE_H__ */
