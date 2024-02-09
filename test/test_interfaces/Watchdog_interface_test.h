a1d60c729f5614c9f2dd7c1e554b4207a31dbf2c
CL16gtgh <ryang359@gatech.edu>
2024-02-09 17:05:53 -0500

    WatchdogInterface tested

--- test/test_interfaces/Watchdog_interface_test.h
+++ test/test_interfaces/Watchdog_interface_test.h
@@ -1,13 +1,19 @@
 #include <Arduino.h>
 #include <unity.h>
 
-#include "WatchdpgInterface.h"
+#include "WatchdogInterface.h"
 
 void test_watchdog_kick()
 {
     // Hypothetical pin setup
     WatchdogInterface wd_interface(1);
-    wd_interface.init(0);
+    wd_interface.init(millis());
     TEST_ASSERT_EQUAL(HIGH, wd_interface.get_watchdog_state());
+    delay(5);
+    wd_interface.kick_watchdog(millis());
+    TEST_ASSERT_EQUAL(HIGH, wd_interface.get_watchdog_state());
+    delay(5);
+    wd_interface.kick_watchdog(millis());
+    TEST_ASSERT_EQUAL(LOW, wd_interface.get_watchdog_state());
 
 }
