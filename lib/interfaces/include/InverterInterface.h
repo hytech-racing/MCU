c2347dbc85a2172aba652414a6272ae59d518af0
walkermburns <walkermburns@gmail.com>
2024-02-09 23:01:32 -0500

    Some cleaning and comments in InverterInterface

--- lib/interfaces/include/InverterInterface.h
+++ lib/interfaces/include/InverterInterface.h
@@ -36,36 +36,51 @@ public:
     // getter for MC id
     uint32_t get_id() { return can_id_; };
 
-    // receive functions
+    /* read energy messages handler from HytechCANInterface */
     void receive_energy_msg(CAN_message_t &msg);
+    /* read status messages handler from HytechCANInterface */
     void receive_status_msg(CAN_message_t &msg);
+    /* read temp messages handler from HytechCANInterface */
     void receive_temp_msg(CAN_message_t &msg);
 
-    // send msg functions
+    /* send MC disable message */
     void disable();
+    /* send MC enable HV message */
     void request_enable_hv();
+    /* send MC enable message */
     void request_enable_inverter();
+    /* send MC command no torque message*/
     void command_no_torque();
+    /* sends torque and rpm setpoints to MC */
     void handle_command(const InverterCommand &command);
+    /* send MC command reset message*/
     void command_reset();
 
+    /* check if MC is ready */
     bool inverter_system_ready()
     {
         return system_ready_;
     }
+
+    /* check for errors */
     bool error()
     {
         return error_;
     }
+
+    /*check for dc quit on */
     bool dc_quit_on()
     {
         return quit_dc_on_;
     }
+
+    /* check for quit inverter on*/
     bool quit_inverter_on()
     {
         return quit_inverter_on_;
     }
 
+    /* check for dc bus voltage */
     uint16_t dc_bus_voltage()
     {
         return dc_bus_voltage_;
@@ -80,6 +95,7 @@ private:
     float id110_val_;                            // for scaling to proper iq and id vals
     float torque_current_, magnetizing_current_; // iq and id in A respectively
     float actual_torque_nm_;
+    /* write setpoints message to the CAN buffer */
     void write_cmd_msg_to_queue_(MC_setpoints_command &msg);
     int16_t speed_;
     uint16_t dc_bus_voltage_;
