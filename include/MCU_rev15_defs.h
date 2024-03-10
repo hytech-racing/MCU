aad427a4c060ae608c17514b45ecf745d7d576ed
walkermburns <walkermburns@gmail.com>
2024-03-09 20:34:00 -0500

    Updated buzzer time and brake/accel thresholds

--- include/MCU_rev15_defs.h
+++ include/MCU_rev15_defs.h
@@ -26,7 +26,7 @@ const int MCU15_FR_LOADCELL_CHANNEL     = 2;
 
 // Time intervals
 const unsigned long SETUP_PRESENT_ACTION_INTERVAL       = 1000;
-const unsigned long BUZZER_ON_INTERVAL                  = 2000;
+const unsigned long BUZZER_ON_INTERVAL                  = 1000;
 const unsigned long INVERTER_ENABLING_TIMEOUT_INTERVAL  = 5000;
 
 // Communication speeds
@@ -34,16 +34,16 @@ const unsigned long INV_CAN_BAUDRATE    = 500000;
 const unsigned long TELEM_CAN_BAUDRATE  = 500000;
 
 // APPS/Brake pedal parameters
-const int ACCEL1_MIN_THRESH = 2170;
+const int ACCEL1_MIN_THRESH = 2160;
 const int ACCEL2_MIN_THRESH = 1550;
 const int ACCEL1_MAX_THRESH = 3340;
 const int ACCEL2_MAX_THRESH = 399;
 const float APPS_ACTIVATION_PERCENTAGE = 0.1;
 
-const int BRAKE1_MIN_THRESH = 2920;
-const int BRAKE2_MIN_THRESH = 770;
-const int BRAKE1_MAX_THRESH = 1500;
-const int BRAKE2_MAX_THRESH = 2200;
+const int BRAKE1_MIN_THRESH = 2890;
+const int BRAKE2_MIN_THRESH = 820;
+const int BRAKE1_MAX_THRESH = 1700;
+const int BRAKE2_MAX_THRESH = 2150;
 const float BRKAE_ACTIVATION_PERCENTAGE = 0.05;
 const float BRAKE_MECH_THRESH = 0.05; // TODO: Determine actual mech threshold
 
