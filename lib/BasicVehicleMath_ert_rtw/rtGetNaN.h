5f65258c15df2bb10b192732941717aa684673f9
shaynoorani <113149316+shaynoorani@users.noreply.github.com>
2024-04-05 21:05:58 -0400

    ya yeet

---
+++ lib/BasicVehicleMath_ert_rtw/rtGetNaN.h
@@ -0,0 +1,51 @@
+//
+// Academic License - for use in teaching, academic research, and meeting
+// course requirements at degree granting institutions only.  Not for
+// government, commercial, or other organizational use.
+//
+// File: rtGetNaN.h
+//
+// Code generated for Simulink model 'BasicVehicleMath'.
+//
+// Model version                  : 1.2
+// Simulink Coder version         : 23.2 (R2023b) 01-Aug-2023
+// C/C++ source code generated on : Fri Apr  5 20:57:25 2024
+//
+// Target selection: ert.tlc
+// Embedded hardware selection: Intel->x86-64 (Windows64)
+// Code generation objectives: Unspecified
+// Validation result: Not run
+//
+#ifndef RTW_HEADER_rtGetNaN_h_
+#define RTW_HEADER_rtGetNaN_h_
+
+extern "C"
+{
+
+#include "rt_nonfinite.h"
+
+}
+
+#include "rtwtypes.h"
+#ifdef __cplusplus
+
+extern "C"
+{
+
+#endif
+
+  extern real_T rtGetNaN(void);
+  extern real32_T rtGetNaNF(void);
+
+#ifdef __cplusplus
+
+}                                      // extern "C"
+
+#endif
+#endif                                 // RTW_HEADER_rtGetNaN_h_
+
+//
+// File trailer for generated code.
+//
+// [EOF]
+//
