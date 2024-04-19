58e312ae9fd60e066f8f7b3c35fc0684f2ec5a43
walkermburns <walkermburns@users.noreply.github.com>
2024-04-19 18:41:25 +0000

    deploy: d12cc962ed714fb5d7397e969e8f6786a05c972d

--- search/functions_0.js
+++ search/functions_0.js
@@ -3,5 +3,9 @@ var searchData=
   ['activate_5fbuzzer_0',['activate_buzzer',['../classBuzzerController.html#a622f82f861f140a939000f257ab3a215',1,'BuzzerController']]],
   ['activecontroller_1',['activeController',['../classTorqueControllerMux.html#a297210cf2e01c3cec82da871a623b25d',1,'TorqueControllerMux']]],
   ['amsinterface_2',['AMSInterface',['../classAMSInterface.html#aa2a8cbda93452f64c5752506fe571080',1,'AMSInterface::AMSInterface(int sw_ok_pin, float init_temp, float init_volt, float temp_alpha, float volt_alpha)'],['../classAMSInterface.html#a384de2c8376bd2040488a2dec2d008b0',1,'AMSInterface::AMSInterface(int sw_ok_pin)'],['../classAMSInterface.html#aa2a8cbda93452f64c5752506fe571080',1,'AMSInterface::AMSInterface(int sw_ok_pin, float init_temp, float init_volt, float temp_alpha, float volt_alpha)']]],
-  ['analogchannel_3',['AnalogChannel',['../classAnalogChannel.html#a9ab26c1e2035a978eb6981bdea5e7987',1,'AnalogChannel']]]
+  ['analogchannel_3',['AnalogChannel',['../classAnalogChannel.html#a9ab26c1e2035a978eb6981bdea5e7987',1,'AnalogChannel']]],
+  ['applyposspeedlimit_4',['applyPosSpeedLimit',['../classTorqueControllerMux.html#a4ad0a150738cb755683dd6169f247293',1,'TorqueControllerMux']]],
+  ['applypowerlimit_5',['applyPowerLimit',['../classTorqueControllerMux.html#a55f8725f93730b3935f683be70685e2d',1,'TorqueControllerMux']]],
+  ['applyregenlimit_6',['applyRegenLimit',['../classTorqueControllerMux.html#a614c7f71cc2cacd73f8ac4abfa434795',1,'TorqueControllerMux']]],
+  ['applytorquelimit_7',['applyTorqueLimit',['../classTorqueControllerMux.html#a5b643ef28baf597d89c41484b880d534',1,'TorqueControllerMux']]]
 ];
