b1d0d31d03b531c144e9ab34230da68b7f7063ed
Luke Chen <43226681+Luke-kC@users.noreply.github.com>
2024-04-20 05:10:42 -0400

    Fixed test config

--- test/test_systems/test_CASE.h
+++ test/test_systems/test_CASE.h
@@ -8,38 +8,42 @@ class FakeMessageQueue
 {
     public:
         FakeMessageQueue() {}
-    
 };
 
-void enqueue_matlab_msg(FakeMessageQueue *, const CAN_MESSAGE_BUS& msg)
+void enqueue_matlab_msg(FakeMessageQueue *, const CAN_MESSAGE_BUS &msg)
 {
-
 }
 
 TEST(CASESystemTesting, test_vn_start_time)
 {
     FakeMessageQueue faked_msg_q;
     CASEConfiguration case_config = {
-        .torqueLimit = 21.4,
+        .torqueLimit = AMK_MAX_TORQUE, // N-m
         .yaw_pid_p = 1.0,
-        .yaw_pid_i = 0.0,
+        .yaw_pid_i = .0,
         .yaw_pid_d = 0.0,
         .tcs_pid_p = 40.0,
         .tcs_pid_i = 0.0,
         .tcs_pid_d = 0.0,
         .useLaunch = false,
         .usePIDTV = true,
+        .useTCSLimitedYawPID = false,
         .useNormalForce = false,
         .useTractionControl = false,
         .usePowerLimit = false,
         .usePIDPowerLimit = false,
-        .tcsThreshold = 0.2,
+        .tcsSLThreshold = 0.2,
         .launchSL = 0.2,
-        .launchDeadZone = 20,
+        .launchDeadZone = 20,        // N-m
+        .launchVelThreshold = 0.75,  // m/s
+        .tcsVelThreshold = 2.5,      // m/s
+        .yawPIDMaxDifferential = 5,  // N-m
+        .yawPIDErrorThreshold = 0.1, // rad/s
+        .yawPIDVelThreshold = 1,     // m/s
+        .yawPIDCoastThreshold = 2.5, // m/s
         .max_rpm = AMK_MAX_RPM,
         .max_regen_torque = MAX_REGEN_TORQUE,
         .max_torque = AMK_MAX_TORQUE,
     };
     CASESystem case_system(&faked_msg_q, 100, 70, case_config);
 }
-
