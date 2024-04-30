21df00adde7c769a4dacc6d054e7a043aa89594d
walkermburns <walkermburns@gmail.com>
2024-04-29 23:45:09 -0400

    fixing merges

--- lib/interfaces/include/EthernetInterface.h
+++ lib/interfaces/include/EthernetInterface.h
@@ -1,23 +1,15 @@
 #ifndef ETHERNETINTERFACE
 #define ETHERNETINTERFACE
+
+#include <Arduino.h>
 #include <QNEthernet.h>
 
-#include "circular_buffer.h"
+EthernetUDP udp_ethernet;
 
-
-
-class EthernetInterface
-{
-public:
-    EthernetInterface() {};
-    void begin(int port);
-    /// @brief sends all udp packets in queue
-    void handle_sending();
-    /// @brief receives all udp packets
-    void handle_recvs();
-
-private:
-    EthernetUDP udp_ethernet_;
-}
+void init_ethernet_device();
+/// @brief sends all udp packets in queue
+void handle_sending();
+/// @brief receives all udp packets
+void handle_recvs();
 
 #endif
