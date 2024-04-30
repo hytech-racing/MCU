458728950403ab1d413db0a25b21555defa4e095
walkermburns <walkermburns@gmail.com>
2024-04-30 14:58:21 -0400

    Initial UDPSocket template

--- lib/interfaces/include/EthernetInterface.h
+++ lib/interfaces/include/EthernetInterface.h
@@ -2,6 +2,7 @@
 #define ETHERNETINTERFACE
 
 #include <Arduino.h>
+#include <stdint.h>
 #include "ht_msgs.pb.h"
 #include "pb_encode.h"
 #include "pb_decode.h"
@@ -10,13 +11,49 @@
 #include <QNEthernet.h>
 
 uint8_t union_port_ethernet_buffer[512];
-// EthernetUDP udp_ethernet;
 
-void init_ethernet_device();
-/// @brief sends all udp packets in queue
-void handle_sending();
-/// @brief receives all udp packets
-void handle_recvs();
+IPAddress ip_addr;
+uint8_t mac_address[6];
+bool connection_successful = false;
+
+void init_ethernet_device(IPAddress ip);
+
+/*
+    The UDPSocket class holds a reference to the
+    individual EthernetUDP socket. It is constructed
+    using a port which the UDP socket will use
+    when it is initialized. 
+    Importantly, this must be done after the ethernet
+    device is initialized with init_ethernet_device();
+*/
+class UDPSocket
+{
+private:
+    EthernetUDP udp_;
+    const uint16_t port_;
+public:
+    UDPSocket(uint16_t port): port_(port) {};
+
+    /* Must be called in setup() to communicate with socket */
+    void initialize_socket() {udp_.begin(port_)}
+
+    /**
+     * @brief Sends a udp packet from the provided buffer
+     * @param data a constant uint8_t pointer to the data buffer
+     * @param len the length of the data in the buffer to send
+     * @return Returns a bool for if the buffer was sent successfully
+    */
+    bool send(const uint8_t *data, int len);
+
+    /**
+     * @brief Receives a udp packet, will return the size of packets received
+     *         and move the data to the location of the buffer pointer passed in
+     * @param data a uint8_t pointer to the data buffer to fill
+     * @return Returns the size of the received packet, or -1 when there
+     *         are no more packets available on the socket
+    */
+    int receive(uint8_t *data);
+}
 
 template <struct interface, struct msg_struct>
 void append_to_interface_queue(interface * msg_interface_queue, msg_struct msg)
