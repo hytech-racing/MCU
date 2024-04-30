cb7c7d86d4f9e9938f0291231b932d660898e62f
Ben Hall <rcmast3r1@gmail.com>
2024-04-30 18:37:04 -0400

    adding stuff in

--- lib/interfaces/include/ProtobufMsgInterface.h
+++ lib/interfaces/include/ProtobufMsgInterface.h
@@ -6,13 +6,17 @@
 #include "pb_decode.h"
 #include "pb_common.h"
 #include "ParameterInterface.h"
+#include "circular_buffer.h"
+
+
+extern Circular_Buffer<uint8_t, (uint32_t)16, sizeof(ht_eth_big_union)> pb_tx_buffer;
 
 struct ETHInterfaces
 {
     ParameterInterface* param_interface;
 };
 
-void handle_pb_stream_union_msg(pb_istream_t *stream, ETHInterfaces& interfaces)
+void recv_pb_stream_union_msg(pb_istream_t *stream, ETHInterfaces& interfaces)
 {
     ht_eth_big_union msg = ht_eth_big_union_init_zero;
     if (pb_decode(stream, ht_eth_big_union_fields, &msg))
@@ -27,5 +31,19 @@ void handle_pb_stream_union_msg(pb_istream_t *stream, ETHInterfaces& interfaces)
         }
     }
 }
+
+template <typename T, uint16_t _size, typename msg_type>
+void send_pb_stream_msg(Circular_Buffer<T, _size, msg_type>& buffer, EthernetInterface *eth_int)
+{
+    while (buffer.available())
+    {
+        msg_type msg;
+        uint8_t buf[sizeof(msg_type)];
+        buffer.pop_front(buf, sizeof(msg_type));
+        memmove(&msg, buf, sizeof(msg));
+        // eth_int->send();
+    }
+}
+
 
 #endif
---
+++ lib/interfaces/src/ProtobufMsgInterface.cpp
@@ -0,0 +1,3 @@
+#include "ProtobufMsgInterface.h"
+
+Circular_Buffer<uint8_t, (uint32_t)16, sizeof(ht_eth_big_union)> pb_tx_buffer;
