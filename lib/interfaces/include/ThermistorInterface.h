c48ee8cb42d0fba4831d761fd98987ecbf84bb76
Eric Galluzzi <ericbgalluzzi@gmail.com>
2024-05-19 20:39:15 -0700

    finalizing interface

--- lib/interfaces/include/ThermistorInterface.h
+++ lib/interfaces/include/ThermistorInterface.h
@@ -4,6 +4,7 @@
 #include "FlexCAN_T4.h"
 #include "HyTech_CAN.h"
 #include "MessageQueueDefine.h"
+#include "AnalogSensorsInterface.h"
 #include "hytech.h"
 
 
@@ -33,14 +34,14 @@ private:
 
 public:
     ThermistorInterface(CANBufferType *msg_output_queue);
-    ThermistorInterface(CANBufferType *msg_output_queue, float beta, uint16_t adc_saturation, float zero_kelvin, float to_celcius, float r_nom, float r0);
+    ThermistorInterface(CANBufferType *msg_output_queue, float beta, uint16_t adc_saturation, float zero_kelvin, float t0_celcius, float r_nom, float r0);
     
     void convert(const AnalogConversion_s &raw_therm_fl, const AnalogConversion_s &raw_therm_fr);
 
     void update_front_thermistor_readings();
     
     template <typename U>
-    void enqueue_new_CAN(U *structure, uint32_t (*pack_function)(U *, uint8_t *, uint8_t *, uint8_t *));
+    void enqueue_CAN_front_thermistors(U *structure, uint32_t (*pack_function)(U *, uint8_t *, uint8_t *, uint8_t *));
     
     void tick(const AnalogConversion_s &raw_therm_fl, const AnalogConversion_s &raw_therm_fr);
 
--- lib/interfaces/src/ThermistorInterface.cpp
+++ lib/interfaces/src/ThermistorInterface.cpp
@@ -1,14 +1,14 @@
 #include "ThermistorInterface.h"
 
 
-ThermistorInterface::ThermistorInterface(CANBufferType *msg_output_queue) 
+ThermistorInterface::ThermistorInterface(CANBufferType *msg_output_queue);
 {
-    this(*msg_output_queue, DEFAULT_THERM_BETA, DEFAULT_ADC_SATUR, DEFAULT_ZERO_KELVIN, DEFAULT_T0_CELCIUS, DEFAULT_R_NOM, DEFAULT_R0);
+    ThermistorInterface(*msg_output_queue, DEFAULT_THERM_BETA, DEFAULT_ADC_SATUR, DEFAULT_ZERO_KELVIN, DEFAULT_T0_CELCIUS, DEFAULT_R_NOM, DEFAULT_R0);
 }
 
-ThermistorInterface::ThermistorInterface(CANBufferType *msg_output_queue, float beta, uint16_t adc_saturation, float zero_kelvin, float to_celcius, float r_nom, float r0)
+ThermistorInterface::ThermistorInterface(CANBufferType *msg_output_queue, float beta, uint16_t adc_saturation, float zero_kelvin, float t0_celcius, float r_nom, float r0);
 {
-    msg_queue = msg_output_queue;
+    msg_queue_ = msg_output_queue;
     _beta = beta;
     _adc_saturation = adc_saturation;
     _zero_kelvin = zero_kelvin;
@@ -35,7 +35,8 @@ void ThermistorInterface::convert(const AnalogConversion_s &raw_therm_fl, const 
     therm_fr = temp_celcius;
 }
 
-float ThermistorInterface::convert(int raw) {
+float ThermistorInterface::convert(int raw) 
+{
     float _t0_kelvin = _t0_celcius + _zero_kelvin;
     float resistance;
     float temp_kelvin;
@@ -47,6 +48,29 @@ float ThermistorInterface::convert(int raw) {
     return temp_celcius;
 }
 
-void ThermistorInterface::update_front_thermistor_readings() {
-    
+void ThermistorInterface::update_front_thermistor_readings() 
+{
+    FRONT_THERMISTORS_t front_thermistors_;
+    //scale by 500 for easy packing
+    front_thermistors_.thermistor_motor_fl = therm_fl * 500;
+    front_thermistors_.thermistor_motor_fr = therm_fr * 500;
+
+    enqueue_CAN_front_thermistors<FRONT_THERMISTORS_t>(&front_thermistors_, &Pack_FRONT_THERMISTORS_hytech);
+}
+
+template<typename U>
+void ThermistorInterface::enqueue_CAN_front_thermistors(U* structure, uint32_t (* pack_function)(U*, uint8_t*, uint8_t*, uint8_t*)) 
+{
+    CAN_message_t can_msg;
+    can_msg.id = pack_function(structure, can_msg.buf, &can_msg.len, (uint8_t*) &can_msg.flags.extended);
+    uint8_t buf[sizeof(CAN_message_t)] = {};
+    memmove(bug, &can_msg, sizeof(CAN_message_t));
+    msg_queue_->push_back(buf, sizeof(CAN_message_t));
+}
+
+void ThermistorInterface::tick(const AnalogConversion_s &raw_therm_fl, const AnalogConversion_s &raw_therm_fr) 
+{
+    therm_fl = convert(raw_therm_fl.raw);
+    therm_fr = convert(raw_therm_fr.raw);
+    update_front_thermistor_readings();
 }
