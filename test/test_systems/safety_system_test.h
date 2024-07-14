e8e4584dd3037ec3e36a5b2e98b672eea861d639
Justin Hwang <justin@goliath.org>
2024-07-14 11:55:22 +0900

    Fixed code to pass test_env systems tests

--- test/test_systems/safety_system_test.h
+++ test/test_systems/safety_system_test.h
@@ -9,33 +9,49 @@
 
 
 TEST(SafetySystemTest, test_software_shutdown) {
-    SysClock sys_clock;
-
-    SysTick_s curr_tick = sys_clock.tick(0);
+    // Initial tick
+    SysTick_s tick_zero;
+    tick_zero.millis = 100L;
+    tick_zero.micros = 100000L;
 
     AMSInterface ams_interface(0, 0, 0, 0, 0, 0);
     WatchdogInterface wd_interface(0);
     SafetySystem safety_system(&ams_interface, &wd_interface);
 
-    ams_interface.init(curr_tick.millis);
-    wd_interface.init(curr_tick.millis);
+    ams_interface.init(tick_zero);
+    wd_interface.init(tick_zero.millis);
     safety_system.init();
 
     ASSERT_EQ(safety_system.get_software_is_ok(), true);
 
     // 10 milliseconds later
-    curr_tick = sys_clock.tick(10 * 1000);
-    ams_interface.set_heartbeat(curr_tick.millis);
+    SysTick_s tick_one;
+    tick_one.millis = 110L;
+    tick_one.micros = 110000L;
+    ams_interface.set_heartbeat(tick_one.millis);
     
-    safety_system.software_shutdown(curr_tick);
+    safety_system.software_shutdown(tick_one);
 
     ASSERT_EQ(safety_system.get_software_is_ok(), true);
 
-    // 45 milliseconds later
-    curr_tick = sys_clock.tick(45 * 1000);
-    safety_system.software_shutdown(curr_tick);
+    // 1999 milliseconds later (should be within 2000ms window)
+    SysTick_s tick_two;
+    tick_two.millis = 2109L;
+    tick_two.micros = 2109000L;
 
-    ASSERT_EQ(ams_interface.heartbeat_received(curr_tick.millis), false);
+    safety_system.software_shutdown(tick_two);
+
+    ASSERT_EQ(ams_interface.heartbeat_received(tick_two.millis), true);
+
+
+    // 1999 milliseconds later (should fail)
+    SysTick_s tick_three;
+    tick_three.millis = 2110L;
+    tick_three.micros = 2110000L;
+
+    safety_system.software_shutdown(tick_three);
+
+    ASSERT_EQ(ams_interface.heartbeat_received(tick_three.millis), false);
 
     ASSERT_EQ(safety_system.get_software_is_ok(), false);
 
