8c64772de38074c223a5d87cffb07686ee87cbf5
Justin Hwang <justin@goliath.org>
2024-08-08 00:04:51 +0900

    Added comments and style for WatchdogInterface

--- lib/interfaces/include/WatchdogInterface.h
+++ lib/interfaces/include/WatchdogInterface.h
@@ -3,37 +3,71 @@
 
 #include <Arduino.h>
 
+/**
+ * The interval at which we will kick the watchdog IC, in milliseconds.
+ * Every WATCHDOG_KICK_INTERVAL, we either write the digital pin high
+ * or low, so our actual period is 2 * WATCHDOG_KICK_INTERVAL.
+ */
 const unsigned long WATCHDOG_KICK_INTERVAL = 10;    // milliseconds
 
+/**
+ * Interface for Watchdog. Through WatchdogInterface, we can kick the watchdog
+ * IC periodically. If the MCU stops sending a kick, then Watchdog will open
+ * the shutdown circuit.
+ */
 class WatchdogInterface
 {
 private:
-    /* Watchdog last kicked time */
-    unsigned long watchdog_time;
+    /**
+     * Watchdog last kicked time, in milliseconds
+     */
+    unsigned long watchdog_time_;
 
-    /* Watchdog output state */
-    bool watchdog_state;
+    /**
+     * Watchdog current state (HIGH or LOW).
+     */
+    bool watchdog_state_;
 
-    /* Hardware interface pins */
+    /**
+     * Hardware interface pins.
+     */
     int pin_watchdog_input_;
     
 public:
-    WatchdogInterface(int wd_input_pin): pin_watchdog_input_(wd_input_pin){};
 
-    /* Initialize interface pin mode */
+    /**
+     * Generic constructor for WatchdogInterface.
+     * @param wd_input_pin The pin on the Teensy that connects to the Watchdog IC.
+     */
+    WatchdogInterface(int wd_input_pin):
+        pin_watchdog_input_(wd_input_pin)
+    {};
+
+    /**
+     * Initialize interface pin mode, sets the watchdog_time_ member variable to
+     * curr_millis, and writes the watchdog_state_ HIGH.
+     */
     void init(unsigned long curr_millis);
 
-    /* Write to Main ECU */
-    // Initialize output value
-    void set_start_state();
+    /**
+     * Writes the watchdog pin HIGH. Does not check if enough time has elapsed.
+     */
+    void set_start_state() {digitalWrite(pin_watchdog_input_, HIGH);}
 
-    /* Kick watchdog */
+    /**
+     * Checks if enough time has elapsed. If so, then this toggles the watchdog_state_
+     * member variable, writes the digital pin appropriately, and updates the
+     * watchdog_time_ member variable.
+     */
     void kick_watchdog(unsigned long curr_millis);
 
-    /* Getters */
-    bool get_watchdog_state();
-    /* Setters */
-    void set_watchdog_state(bool state);
+    /* ---------- Getters ---------- */
+    bool get_watchdog_state() {return watchdog_state_;}
+    unsigned long get_watchdog_time() {return watchdog_time_;}
+
+    /* ---------- Setters ---------- */
+    void set_watchdog_state(bool state) {watchdog_state_ = state;}
+    void set_watchdog_time(unsigned long time) {watchdog_time_ = time;}
 
 };
 
--- lib/interfaces/src/WatchdogInterface.cpp
+++ lib/interfaces/src/WatchdogInterface.cpp
@@ -1,40 +1,21 @@
 #include "WatchdogInterface.h"
 
-/* Pin mode output to watchdog WD */
-void WatchdogInterface::init(unsigned long curr_millis) {
+void WatchdogInterface::init(unsigned long curr_millis)
+{
     // Set pin mode        
     pinMode(pin_watchdog_input_, OUTPUT);
 
-    watchdog_time = curr_millis;
+    watchdog_time_ = curr_millis;
     set_watchdog_state(HIGH);
-
 }
 
-/* Initial output to watchdog WD */
-void WatchdogInterface::set_start_state() {
+void WatchdogInterface::kick_watchdog(unsigned long curr_millis)
+{
 
-    digitalWrite(pin_watchdog_input_, HIGH);
-    
-}
-
-/* Toggle watchdog WD to kick dog */
-void WatchdogInterface::kick_watchdog(unsigned long curr_millis) {
-
-    if ((curr_millis - watchdog_time) > WATCHDOG_KICK_INTERVAL) {
-        watchdog_state = !watchdog_state;
-        digitalWrite(pin_watchdog_input_, watchdog_state);
-        watchdog_time = curr_millis;
+    if ((curr_millis - watchdog_time_) > WATCHDOG_KICK_INTERVAL) {
+        set_watchdog_state(!watchdog_state_);
+        digitalWrite(pin_watchdog_input_, watchdog_state_);
+        set_watchdog_time(curr_millis);
     }
 
 }
-
-/* Get interface status */
-bool WatchdogInterface::get_watchdog_state() {
-    return watchdog_state;
-}
-
-/* Set interface status */
-void WatchdogInterface::set_watchdog_state(bool state) {
-    watchdog_state = state;
-}
-
