713177e4846d09b4b2790d7e295acb22eef7aa0c
Justin Hwang <justin@goliath.org>
2024-09-08 20:19:43 -0400

    Updated documentation for MCUInterface.h

--- lib/interfaces/include/MCUInterface.h
+++ lib/interfaces/include/MCUInterface.h
@@ -1,13 +1,19 @@
 #ifndef __MCU_INTERFACE_H__
 #define __MCU_INTERFACE_H__
 
+/* Standard Includes */
 #include <stdint.h>
+
+/* Library Includes */
 #include "FlexCAN_T4.h"
 #include "HyTech_CAN.h"
 #include "hytech.h"
+
+/* System Includes */
 #include "MessageQueueDefine.h"
 #include "PedalsSystem.h"
 
+/* Constant definitions */
 const int DEFAULT_BMS_OK_READ         = 17;      // SHDN_D_READ
 const int DEFAULT_BMS_SENSE_PIN       = 16;      // BMS_OK_SENSE
 const int DEFAULT_IMD_OK_READ         = 10;      // SHDN_C_READ
@@ -16,12 +22,14 @@ const int DEFAULT_BSPD_OK_READ        = 39;      // SHDN_E_READ
 const int DEFAULT_SOFTWARE_OK_READ    = 25;      // SHDN_F_READ Watchdog Combined
 const int DEFAULT_BOTS_OK_READ        = 24;      // SHDN_B_READ
 const int DEFAULT_BRB_OK_READ         = 26;      // SHDN_G_READ
-const int DEFAULT_BRAKE_LIGHT_CTRL    = 6;
-const int DEFAULT_INVERTER_ENABLE     = 9;
-const int DEFAULT_INVERTER_24V_ENABLE = 7;
+const int DEFAULT_BRAKE_LIGHT_CTRL    = 6;       // Pin for controlling brake lights
+const int DEFAULT_INVERTER_ENABLE     = 9;       // ENABLE pin for inverters
+const int DEFAULT_INVERTER_24V_ENABLE = 7;       // ENABLE pin for inverters' 24V
 
-/// @brief specifically designed so that Walker would be happy
-struct MainECUHardwareReadPins
+/**
+ * Struct to keep track of MCU pinouts.
+ */
+struct MainECUHardwareReadPins_s
 {
     // shutdown read pins
     int pin_bms_ok_read;
@@ -37,88 +45,130 @@ struct MainECUHardwareReadPins
     int pin_inv_24V_en;
 };
 
-static const MainECUHardwareReadPins DEFAULT_PINS = {DEFAULT_BMS_OK_READ,DEFAULT_IMD_OK_READ, DEFAULT_BSPD_OK_READ, DEFAULT_SOFTWARE_OK_READ,
+/**
+ * Default MCU pinouts.
+ */
+static const MainECUHardwareReadPins_s DEFAULT_PINS = {DEFAULT_BMS_OK_READ,DEFAULT_IMD_OK_READ, DEFAULT_BSPD_OK_READ, DEFAULT_SOFTWARE_OK_READ,
                                                      DEFAULT_BOTS_OK_READ, DEFAULT_BRB_OK_READ, DEFAULT_BRAKE_LIGHT_CTRL, DEFAULT_INVERTER_ENABLE, DEFAULT_INVERTER_24V_ENABLE};
 
+/**
+ * Class representing the MCU interface, containing all the pinouts and sensing.
+ */
 class MCUInterface
 {
 private:
+    /**
+     * Circular buffer for the CAN message queue. The MCU repo can enqueue messages onto the Telem CAN using
+     * this message queue.
+     */
     CANBufferType *msg_queue_;
 
-    MainECUHardwareReadPins pins_;
+    /**
+     * MainECUHardwareReadPins_s struct for this object's pinout.
+     */
+    MainECUHardwareReadPins_s pins_;
 
-    /* Outbound CAN message */
+    /**
+     * Outbound MCU_STATUS message.
+     */
     MCU_STATUS_t mcu_status_;
 
-    /* Shutdown circuit input */
-    bool bms_ok_high;
-    bool imd_ok_high;
-    bool bspd_ok_high;
-    bool software_ok_high;
-    bool brb_ok_high;
-    /* Shutdown circuit voltage */ 
-    bool shutdown_b_above_threshold; // BOTS
-    bool shutdown_c_above_threshold; // IMD
-    bool shutdown_d_above_threshold; // AMS
-    bool shutdown_e_above_threshold; // BSPD
-    bool shutdown_g_above_threshold; // BRB
+    /**
+     * Inputs from the SHDN circuit. These booleans get updated in 
+     */
+    bool bms_ok_high_;
+    bool imd_ok_high_;
+    bool bspd_ok_high_;
+    bool software_ok_high_;
+    bool brb_ok_high_;
 
-    /* Private utility functions */
-    // Read all shutdown signals on ECU
+    /**
+     * Shutdown circuit to check if each shutdown line is high.
+     */ 
+    bool shutdown_b_above_threshold_; // BOTS
+    bool shutdown_c_above_threshold_; // IMD
+    bool shutdown_d_above_threshold_; // AMS
+    bool shutdown_e_above_threshold_; // BSPD
+    bool shutdown_g_above_threshold_; // BRB
+
+    /**
+     * Measures the inputs into the shutdown relays. These are all digital signals.
+     * 
+     * bms_ok_high_, imd_ok_high_, bspd_ok_high_, software_ok_high_, and brb_ok_high_
+     */
     void measure_shutdown_circuit_input();
+
+    /**
+     * Measures whether or not the shutdown circuit voltages are above the threshold.
+     * These are very similar to the outputs from measure_shutdown_circuit_input(), but
+     * some are slightly different.
+     */ 
     void measure_shutdown_circuit_voltage();
-    // Update CAN message content
+
+    /**
+     * Updates the MCU_STATUS member variable from the other boolean member variables.
+     */
     void update_mcu_status_CAN();
 
 public:
-    // PLEASE replace these long lists of parameters with structs
-    // and put initialization in constructor body instead of initializer list     -- happy?
-    // my retinas are in pain
-    MCUInterface(CANBufferType *msg_output_queue, const MainECUHardwareReadPins &pins):
-        // Member initialization list
+
+    /**
+     * Constructor for creating a new MCUInterface object. This required a struct specifying the
+     * MCU pinout.
+     */
+    MCUInterface(CANBufferType *msg_output_queue, const MainECUHardwareReadPins_s &pins):
         msg_queue_(msg_output_queue),
         pins_(pins){};
 
-    // Overloading constructor
+    /**
+     * Overloaded constructor that uses the DEFAULT_PINS by default.
+     */
     MCUInterface(CANBufferType *msg_output_queue):        
         MCUInterface(msg_output_queue, DEFAULT_PINS)
     {};
 
-    /* Initialize shutdown circuit input readings */
+    /**
+     * Initalization function to set the pin mode and initialize the OK flags to FALSE.
+     */
     void init();
 
-    /* Read from Main ECU */
+    /**
+     * Calls the measure_shutdown_circuit_input() and measure_shutdown_circuit_voltage() functions
+     * to update the member variables from the digital pins.
+     */
     void read_mcu_status();
 
-    /* Write to Main ECU */
+    /**
+     * Calls digitalWrite() on the brake light pin with the given boolean.
+     */ 
     void set_brake_light(bool brake_pedal_is_active); // Called from PedalInterface/System
+
+    /**
+     * Calls digitalWrite() on the inverter enable and inverter 24V enable pins.
+     */
     void enable_inverters_pin();
+
+    /**
+     * Calls digitalWrite() on the inverter enable and inverter 24V enable pins.
+     */
     void disable_inverters_pin();
 
-    /* Feed to state machine */
+    /**
+     * Getter for bms_ok_high_, imd_ok_high_, brb_ok_high_, and bots_ok_.
+     */
     bool bms_ok_is_high();
     bool imd_ok_is_high();
     bool brb_ok_is_high();
-
     bool get_bots_ok();
 
-    /* Update MCU_status CAN (main loop) */
-    // State machine
-    void update_mcu_status_CAN_fsm(int fsm_state);
-    // Systems
-    void update_mcu_status_CAN_drivetrain(bool has_error);
-    void update_mcu_status_CAN_safety(bool is_ok);
-    void update_mcu_status_CAN_TCMux(int drive_mode, int torque_mode, float max_torque);
-    void update_mcu_status_CAN_buzzer(bool is_on);
-    void update_mcu_status_CAN_pedals(const PedalsSystemData_s &pedals);
-    // Interfaces
-    void update_mcu_status_CAN_ams(bool is_critical);
-    void update_mcu_status_CAN_dashboard(bool is_active);
-
-    /* Enqueue MCU_status CAN  */
+    /**
+     * Enqueue MCU_status CAN
+     */
     void enqueue_CAN_mcu_status();
 
-    /* Tick MCUInterface at 10HZ */
+    /**
+     * Tick MCUInterface at 10HZ
+     */
     void tick(
         int fsm_state,
         bool inv_has_error,
--- lib/interfaces/src/MCUInterface.cpp
+++ lib/interfaces/src/MCUInterface.cpp
@@ -1,8 +1,5 @@
 #include "MCUInterface.h"
 
-/* Member functions */
-
-/* Initialize shutdown circuit input readings */
 void MCUInterface::init()
 {
     // Set pin mode
@@ -11,50 +8,38 @@ void MCUInterface::init()
     pinMode(pins_.pin_brake_light_ctrl, OUTPUT);
 
     // Set initial shutdown circuit readings
-    bms_ok_high = false;
-    imd_ok_high = false;
-    brb_ok_high = false;
-
-    // Enable inverters (?)
-    // Should be called from drivetrain
+    bms_ok_high_ = false;
+    imd_ok_high_ = false;
+    brb_ok_high_ = false;
 }
 
-/* Read shutdown system values (Not ticked, just in main loop)*/
 void MCUInterface::read_mcu_status()
 {
-
     measure_shutdown_circuit_input();
     measure_shutdown_circuit_voltage();
 }
 
-/* Measure shutdown circuits' input */
 void MCUInterface::measure_shutdown_circuit_input()
 {
-    // TODO: change these back to pins from constructor
-    bms_ok_high = digitalRead(DEFAULT_BMS_SENSE_PIN);
-    imd_ok_high = digitalRead(DEFAULT_IMD_SENSE_PIN);
+    bms_ok_high_ = digitalRead(DEFAULT_BMS_SENSE_PIN);
+    imd_ok_high_ = digitalRead(DEFAULT_IMD_SENSE_PIN);
     
-    bspd_ok_high = digitalRead(pins_.pin_bspd_ok_read);
-    software_ok_high = digitalRead(pins_.pin_software_ok_read);
-    brb_ok_high = digitalRead(pins_.pin_brb_ok_read);
-
+    bspd_ok_high_ = digitalRead(pins_.pin_bspd_ok_read);
+    software_ok_high_ = digitalRead(pins_.pin_software_ok_read);
+    brb_ok_high_ = digitalRead(pins_.pin_brb_ok_read);
 }
 
-/* Measure shutdown circuits' voltages */
 void MCUInterface::measure_shutdown_circuit_voltage()
 {
-
-    shutdown_b_above_threshold = digitalRead(pins_.pin_bots_ok_read);
-    shutdown_c_above_threshold = digitalRead(pins_.pin_imd_ok_read);
-    shutdown_d_above_threshold = digitalRead(pins_.pin_bms_ok_read);
-    shutdown_e_above_threshold = digitalRead(pins_.pin_bspd_ok_read);
-    shutdown_g_above_threshold = digitalRead(pins_.pin_brb_ok_read);
+    shutdown_b_above_threshold_ = digitalRead(pins_.pin_bots_ok_read);
+    shutdown_c_above_threshold_ = digitalRead(pins_.pin_imd_ok_read);
+    shutdown_d_above_threshold_ = digitalRead(pins_.pin_bms_ok_read);
+    shutdown_e_above_threshold_ = digitalRead(pins_.pin_bspd_ok_read);
+    shutdown_g_above_threshold_ = digitalRead(pins_.pin_brb_ok_read);
 }
 
-/* Write brake light */
 void MCUInterface::set_brake_light(bool brake_pedal_is_active)
 {
-
     digitalWrite(pins_.pin_brake_light_ctrl, brake_pedal_is_active);
 }
 
@@ -68,31 +53,27 @@ void MCUInterface::disable_inverters_pin()
     digitalWrite(pins_.pin_inv_en, LOW);
     digitalWrite(pins_.pin_inv_24V_en, LOW);
 }
-/* Shutdown circuit input state */
-// BMS
+
 bool MCUInterface::bms_ok_is_high()
 {
-    return bms_ok_high;
-}
-// OKHS (IMD)
-bool MCUInterface::imd_ok_is_high()
-{
-    return imd_ok_high;
+    return bms_ok_high_;
 }
 
-// BRB
+bool MCUInterface::imd_ok_is_high()
+{
+    return imd_ok_high_;
+}
+
 bool MCUInterface::brb_ok_is_high()
 {
-    return brb_ok_high;
+    return brb_ok_high_;
 }
 
 bool MCUInterface::get_bots_ok()
 {
-    return shutdown_b_above_threshold;
+    return shutdown_b_above_threshold_;
 }
 
-/* Send CAN message */
-// MCU status
 void MCUInterface::enqueue_CAN_mcu_status()
 {
     CAN_message_t msg;
@@ -104,83 +85,18 @@ void MCUInterface::enqueue_CAN_mcu_status()
     msg_queue_->push_back(buf, sizeof(CAN_message_t));
 }
 
-/* Update MCU_status CAN */
-// MCUInterface
 void MCUInterface::update_mcu_status_CAN()
 {
     // Shutdown circuit input
-    mcu_status_.bms_ok_high = bms_ok_high;
-    mcu_status_.imd_ok_high = imd_ok_high;
-    mcu_status_.bspd_ok_high = bspd_ok_high;
-    mcu_status_.software_ok_high = software_ok_high;
+    mcu_status_.bms_ok_high = bms_ok_high_;
+    mcu_status_.imd_ok_high = imd_ok_high_;
+    mcu_status_.bspd_ok_high = bspd_ok_high_;
+    mcu_status_.software_ok_high = software_ok_high_;
     // Shutdown circuit voltage
-    mcu_status_.shutdown_b_above_threshold = shutdown_b_above_threshold;
-    mcu_status_.shutdown_c_above_threshold = shutdown_c_above_threshold;
-    mcu_status_.shutdown_d_above_threshold = shutdown_d_above_threshold;
-    mcu_status_.shutdown_e_above_threshold = shutdown_e_above_threshold;
-    
-}
-
-// Main loop
-// State machine
-void MCUInterface::update_mcu_status_CAN_fsm(int fsm_state_enum_val)
-{
-    mcu_status_.ecu_state = fsm_state_enum_val;
-}
-// DriveTrain
-void MCUInterface::update_mcu_status_CAN_drivetrain(bool has_error)
-{
-    // Drivetrain returns struct in main loop
-    // drivetrain.drive_error_occured()
-    mcu_status_.inverter_error = has_error; // could also be called has_error
-}
-// SafetySystem
-void MCUInterface::update_mcu_status_CAN_safety(bool is_ok)
-{
-    // SafetySystem returns struct in main loop
-    // safety_system.get_software_is_ok()
-    mcu_status_.software_ok = is_ok;
-}
-// AMSInterface
-void MCUInterface::update_mcu_status_CAN_ams(bool is_critical)
-{
-    // AMSInterface returns struct in main loop
-    // ams_interface.pack_charge_is_critical()
-    mcu_status_.pack_charge_critical = is_critical;
-}
-// TorqueControllerMux
-// Would need an agreement on
-void MCUInterface::update_mcu_status_CAN_TCMux(int drive_mode, int torque_mode, float max_torque)
-{
-    // TorqueControllerMux returns struct in main loop
-    mcu_status_.drive_mode = drive_mode;
-    mcu_status_.torque_mode = torque_mode;
-    mcu_status_.max_torque = static_cast<uint8_t>(max_torque);
-}
-// DashboardInterface
-void MCUInterface::update_mcu_status_CAN_dashboard(bool is_active)
-{
-    // DashboardInterface (?) returns struct in main loop
-    // dash.lauchControlButtonPressed()
-    mcu_status_.launch_control_active = is_active;
-}
-// BuzzerSystem
-void MCUInterface::update_mcu_status_CAN_buzzer(bool is_on)
-{
-    // Buzzer returns struct in main loop
-    // buzzer.buzzer_is_on()
-    mcu_status_.drive_buzzer = is_on;
-}
-// PedalSystem
-// Would need to agree on
-void MCUInterface::update_mcu_status_CAN_pedals(const PedalsSystemData_s &pedals)
-{
-    // PedalSystem returns struct in main loop
-    mcu_status_.brake_pedal_active = pedals.brakePressed;
-    // mcu_status_.set_mech_brake_active();
-    mcu_status_.no_accel_implausibility = !pedals.accelImplausible;
-    mcu_status_.no_brake_implausibility = !pedals.brakeImplausible;
-    mcu_status_.no_accel_or_brake_implausibility = !(pedals.brakeAndAccelPressedImplausibility);
+    mcu_status_.shutdown_b_above_threshold = shutdown_b_above_threshold_;
+    mcu_status_.shutdown_c_above_threshold = shutdown_c_above_threshold_;
+    mcu_status_.shutdown_d_above_threshold = shutdown_d_above_threshold_;
+    mcu_status_.shutdown_e_above_threshold = shutdown_e_above_threshold_;    
 }
 
 void MCUInterface::tick(int fsm_state,
@@ -194,19 +110,28 @@ void MCUInterface::tick(int fsm_state,
                         bool pack_charge_is_critical,
                         bool button_is_pressed)
 {
-    // State machine
-    update_mcu_status_CAN_fsm(fsm_state);
-    // Systems
-    update_mcu_status_CAN_drivetrain(inv_has_error);
-    update_mcu_status_CAN_safety(software_is_ok);
-    update_mcu_status_CAN_TCMux(drive_mode, torque_mode, max_torque);
-    update_mcu_status_CAN_buzzer(buzzer_is_on);
-    update_mcu_status_CAN_pedals(pedals_data);
-    // External Interfaces
-    update_mcu_status_CAN_ams(pack_charge_is_critical);
-    update_mcu_status_CAN_dashboard(button_is_pressed);
-    // Internal values
+    mcu_status_.ecu_state = fsm_state;
+
+    mcu_status_.inverter_error = inv_has_error;
+    mcu_status_.software_ok = software_is_ok;
+    
+    mcu_status_.drive_mode = drive_mode;
+    mcu_status_.torque_mode = torque_mode;
+    mcu_status_.max_torque = static_cast<uint8_t>(max_torque);
+    
+    mcu_status_.drive_buzzer = buzzer_is_on;
+    
+    mcu_status_.brake_pedal_active = pedals_data.brakePressed;
+    mcu_status_.no_accel_implausibility = !pedals_data.accelImplausible;
+    mcu_status_.no_brake_implausibility = !pedals_data.brakeImplausible;
+    mcu_status_.no_accel_or_brake_implausibility = !(pedals_data.brakeAndAccelPressedImplausibility);
+    
+    mcu_status_.pack_charge_critical = pack_charge_is_critical;
+
+    mcu_status_.launch_control_active = button_is_pressed;
+
     update_mcu_status_CAN();
+
     // Push into buffer
     enqueue_CAN_mcu_status();
     set_brake_light(pedals_data.brakePressed);
