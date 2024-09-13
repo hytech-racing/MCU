411149b28f1ff6021c177680667680e20037c749
Ben Hall <rcmast3r1@gmail.com>
2024-09-13 19:48:45 -0400

    removing non-used function

--- lib/systems/include/BaseController.h
+++ lib/systems/include/BaseController.h
@@ -10,10 +10,6 @@ namespace BaseControllerParams
 }
 class Controller
 {
-private:
-    // common function for calculation of the limited torque request that goes into controllers
-    float get_torque_request_(float torque_limit_nm, const PedalsSystemData_s &pedals_data) { return 0; };
-
 public:
     virtual TorqueControllerOutput_s evaluate(const SharedCarState_s &state) = 0;
 };
