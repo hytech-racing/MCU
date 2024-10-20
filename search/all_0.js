0ec6cd923995b42995e48d6f1067e2f733f90a51
RCMast3r <RCMast3r@users.noreply.github.com>
2024-10-20 20:29:50 +0000

    deploy: 10775c6d2646cf8a29b1351bf32fc579e857be64

--- search/all_0.js
+++ search/all_0.js
@@ -3,8 +3,11 @@ var searchData=
   ['_5fdata_0',['_data',['../classDashboardInterface.html#a4c04f44e4b3306baa223d937b10fec86',1,'DashboardInterface']]],
   ['_5ffront_5fthermistors_1',['_front_thermistors',['../classThermistorInterface.html#a8e5a02f5d59ae6b06d8255e8437c93ff',1,'ThermistorInterface']]],
   ['_5flast_5fsetpoint_5fmillis_2',['_last_setpoint_millis',['../classDrivebrainController.html#ad0442afcd18875f86aed43a1f7b5fcf1',1,'DrivebrainController']]],
-  ['_5flatest_5fdata_3',['_latest_data',['../classDrivebrainETHInterface.html#a9c94a6ead8f5080da0fb99803f4abead',1,'DrivebrainETHInterface']]],
-  ['_5fmsg_5fqueue_4',['_msg_queue',['../classThermistorInterface.html#a0fa04ececefbe2f7f868549556e98b00',1,'ThermistorInterface']]],
-  ['_5fparams_5',['_params',['../classDrivebrainController.html#a59c55aa60560d06913fbf781c0dc4b14',1,'DrivebrainController']]],
-  ['_5ftiming_5ffailure_6',['_timing_failure',['../classDrivebrainController.html#a0c1461ea45576b27200ae52702ce164b',1,'DrivebrainController']]]
+  ['_5flast_5fworst_5flatency_5ftimestamp_3',['_last_worst_latency_timestamp',['../classDrivebrainController.html#a77bc1ea0a687a78636497073d8a5ed4b',1,'DrivebrainController']]],
+  ['_5flatest_5fdata_4',['_latest_data',['../classDrivebrainETHInterface.html#a9c94a6ead8f5080da0fb99803f4abead',1,'DrivebrainETHInterface']]],
+  ['_5fmsg_5fqueue_5',['_msg_queue',['../classThermistorInterface.html#a0fa04ececefbe2f7f868549556e98b00',1,'ThermistorInterface']]],
+  ['_5fparams_6',['_params',['../classDrivebrainController.html#a59c55aa60560d06913fbf781c0dc4b14',1,'DrivebrainController']]],
+  ['_5fraw_5fdata_7',['_raw_data',['../classLoadCellInterface.html#a5b512d1ea97fc9b6c8dd3a043dace3e3',1,'LoadCellInterface']]],
+  ['_5ftiming_5ffailure_8',['_timing_failure',['../classDrivebrainController.html#a0c1461ea45576b27200ae52702ce164b',1,'DrivebrainController']]],
+  ['_5fworst_5flatency_5fso_5ffar_9',['_worst_latency_so_far',['../classDrivebrainController.html#aea1586b58b7618b869028df6369a6440',1,'DrivebrainController']]]
 ];
--- search/classes_7.js
+++ search/classes_7.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['loadcellinterface_0',['LoadCellInterface',['../classLoadCellInterface.html',1,'']]],
   ['loadcellinterfaceoutput_5fs_1',['LoadCellInterfaceOutput_s',['../structLoadCellInterfaceOutput__s.html',1,'']]],
-  ['loadcellinterfacetick_5fs_2',['LoadCellInterfaceTick_s',['../structLoadCellInterfaceTick__s.html',1,'']]]
+  ['loadcellinterfacerawoutput_5fs_2',['LoadCellInterfaceRawOutput_s',['../structLoadCellInterfaceRawOutput__s.html',1,'']]],
+  ['loadcellinterfacetick_5fs_3',['LoadCellInterfaceTick_s',['../structLoadCellInterfaceTick__s.html',1,'']]]
 ];
