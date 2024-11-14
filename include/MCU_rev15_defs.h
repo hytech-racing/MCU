23daf8fd92f4d5ead40d92e5d3853de2d34b5684
Ben Hall <rcmast3r1@gmail.com>
2024-11-14 15:40:46 -0500

    updated pedal params

--- include/MCU_rev15_defs.h
+++ include/MCU_rev15_defs.h
@@ -52,17 +52,17 @@ const unsigned long TELEM_CAN_BAUDRATE  = 500000;
 // All of these values are the PEDAL min/max
 // the sensor min/max that trip implaus are calculated
 // in the PedalsSystem constructor
-const int ACCEL1_PEDAL_MAX = 3490;  
-const int ACCEL2_PEDAL_MAX = 189;   
+const int ACCEL1_PEDAL_MAX = 3491;  
+const int ACCEL2_PEDAL_MAX = 225;   
 
-const int ACCEL1_PEDAL_MIN = 2646;
-const int ACCEL2_PEDAL_MIN = 1030;
+const int ACCEL1_PEDAL_MIN = 2741;
+const int ACCEL2_PEDAL_MIN = 966;
 
-const int BRAKE1_PEDAL_MAX = 2022;  
-const int BRAKE2_PEDAL_MAX = 1668;  
+const int BRAKE1_PEDAL_MAX = 1878;  
+const int BRAKE2_PEDAL_MAX = 1811;  
 
-const int BRAKE1_PEDAL_MIN = 1192;  
-const int BRAKE2_PEDAL_MIN = 2476;
+const int BRAKE1_PEDAL_MIN = 1143;  
+const int BRAKE2_PEDAL_MIN = 2526;
 
 const int ACCEL1_PEDAL_OOR_MIN = 90;
 const int ACCEL2_PEDAL_OOR_MIN = 90;
